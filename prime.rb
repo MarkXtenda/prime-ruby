6e68875d2f631531775c3de437f66cf28e89ac8f
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-02 22:01:02 +1000

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+    prime = [number]
+
+    index = number - 1
+    while index > 0
+        (number % index == 0) ? prime << index : nil
+        index -= 1
+    end
+    (prime.size > 2 || number <= 0 || number == 1) ? false : true
+end
+
+puts prime?(1)
